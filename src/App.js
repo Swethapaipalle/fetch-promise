dae3e4812db9bb942acf56e6bf289730fd1126c6
Swethapaipalle <swethapaipalle87@gmail.com>
2022-11-11 15:25:10 -0500

    fetch data using fetch and promise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+export default function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const apiData=fetch(`https://jsonplaceholder.typicode.com/users`).then((response) =>
+      response.json()
+    );
+    console.log("apiData",apiData)
+    const results=Promise.all([apiData]).then(res=>{setData(res[0])})
+  
+  }, []);
+console.log("data", data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        {data.map((item, index) => {
+          console.log("item", item);
+          return (
+            <tr key={index}>
+              <td>{item.username}</td>
+              <td>{item.email}</td>
+            </tr>
+          );
+        })}
+      </table>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
-
-export default App;
